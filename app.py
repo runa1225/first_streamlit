0dec1ff8a807ea309929394fae9133590d8e9bc1
runa1225 <G022C1013@g.neec.ac.jp>
2024-07-04 17:17:50 +0900

    Update app.py

--- app.py
+++ app.py
@@ -5,15 +5,8 @@ import matplotlib.pyplot as plt
 st.title("投資シミュレーションツール")
 
 invest_per_month = st.number_input("毎月の投資額を入力してください（単位：万円）", value=5.0, format="%.1f")
-
 ages = st.slider('投資開始年齢と終了年齢を指定してください', 0, 100, (25, 60))
-
 interest_rate = st.number_input("想定利回りを入力してください。（単位：%）", value=5.0, format="%.1f")
-
-# デバッグ用の出力
-st.write(f"毎月の投資額: {invest_per_month}万円")
-st.write(f"投資開始年齢と終了年齢: {ages}")
-st.write(f"想定利回り: {interest_rate}%")
 
 if invest_per_month and interest_rate:
     # 年間投資額
@@ -30,12 +23,6 @@ if invest_per_month and interest_rate:
 
     # 複利によるトータル資産の作成
     total = [invest_per_month * (((1 + (interest_rate / 100 / 12)) ** (i * 12) - 1) / (interest_rate / 100 / 12)) for i in period]
-
-    # 変数が正しく定義されているか確認
-    st.write(f"年齢リスト: {age}")
-    st.write(f"投資期間: {period}")
-    st.write(f"総投資額: {savings}")
-    st.write(f"トータル資産: {total}")
 
     df = pd.DataFrame({'age': age, 'period': period, 'savings': savings, 'total': total})
 
